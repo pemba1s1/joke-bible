64295f123d66dac65b8f04f0d72f8a5be1ae2aa6
pemba1s1 <norsangpemba@gmail.com>
2022-09-09 20:36:38 +0545

    redirect if only one search result

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { SearchContext } from '../../Context/useSearchContext';
 import Lightning from '../../assets/assets_Homework_Front-End_01/green-light-copy-2.png';
 
 export const SearchBar = () => {
   const { searchKey, setSearchKey, searchList } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  const onlyOneResult = (searchList) => {
+    setSearchKey('');
+    navigate(`/joke/${searchList[0].id}&0`);
+  };
   return (
     <div className="relative">
       <div className="searchbar">
@@ -12,10 +18,12 @@ export const SearchBar = () => {
           type="text"
           placeholder="How can we make you laugh today?"
           name="search"
+          value={searchKey}
           onChange={(e) => setSearchKey(e.target.value)}
         />
       </div>
       <div className={`search-list ${searchKey !== '' && searchList.length > 0 ? '' : 'display-none'}`}>
+        {searchList.length === 1 && onlyOneResult(searchList)}
         {searchList.map((item) => (
           <Link to={`/joke/${item.id}&0`} key={item.id}>
             <img src={Lightning} alt="x"></img>
