f9be629b15fdb2d8a2c67746fba315bf9f172ad1
pemba1s1 <norsangpemba@gmail.com>
2022-09-09 17:02:56 +0545

    nav bar dropdown fix

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import User from '../../assets/assets_Homework_Front-End_01/shape.png';
 import IconDown from '../../assets/assets_Homework_Front-End_01/path_2.png';
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="navcontainer">
       <div className="navmenu">
@@ -13,12 +14,16 @@ const Navbar = () => {
         <NavLink className="navmenuitem" to="#">
           SONDERANGEBOTE
         </NavLink>
-        <div className="dropdown navmenuitem">
-          <div className="dropbtn">
-            <img src={User} className="icon"></img>MEIN BEREICH
-            <img src={IconDown} className="icon"></img>
+        <div className="dropdown navmenuitem cursor-pointer" onClick={() => setClicked(!clicked)}>
+          <div className="dropbtn flex space-between">
+            <div>
+              <img src={User} className="icon" alt="a"></img>MEIN BEREICH
+            </div>
+            <div>
+              <img src={IconDown} className="icon" alt="a"></img>
+            </div>
           </div>
-          <div className="dropdown-content">
+          <div className={`dropdown-content ${clicked ? '' : 'display-none'}`}>
             <NavLink className="submenuitems" to="#">
               My published jokes
             </NavLink>
